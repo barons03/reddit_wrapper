b7bf9946c21954e2c0a403125bb41115c0697fb1
nkyian <nkyian@fastmail.com>
2018-11-20 13:27:31 +0200

    Updated gitignore and added hex.pm config

--- mix.exs
+++ mix.exs
@@ -6,8 +6,27 @@ defmodule RedditWrapper.MixProject do
       app: :reddit_wrapper,
       version: "0.1.0",
       elixir: "~> 1.7",
+      description: description(),
+      package: package(),
+      name: "reddit_wrapper",
+      source_url: "https://github.com/nkyian/reddit_wrapper",
       start_permanent: Mix.env() == :prod,
       deps: deps()
+    ]
+  end
+
+  defp description() do
+    "Reddit API Wrapper"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "reddit_wrapper",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs LICENSE*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/nkyian/reddit_wrapper"}
     ]
   end
 
@@ -28,7 +47,8 @@ defmodule RedditWrapper.MixProject do
       {:jason, "~> 1.1"},
       # Validation support
       {:vex, "~> 0.8.0"},
-      {:exconstructor, "~> 1.1.0"}
+      {:exconstructor, "~> 1.1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
