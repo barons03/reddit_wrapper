a696a6f99ce0f0c6a64dcd86b76fa3d68efee6df
barons03 <bkrisjanis@gmail.com>
2019-02-25 19:10:37 +0200

    Initial commit

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RedditWrapper.MixProject do
   def project do
     [
       app: :reddit_wrapper,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.7",
       description: description(),
       package: package(),
